477237a028d8df0eb3f0650180824e53d93be929
zjmitche <zjmitche@gmail.com>
2019-01-01 21:02:29 -0700

    npm updates

--- src/App/SearchForm.js
+++ src/App/SearchForm.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
+import './SearchForm.css';
 
 class SearchForm extends Component {
   render() {
     return (
-      <form className="search-form">
-        <input 
-          name="search"
-          type="text"
-          placeholder="Web Developer"
-        />
-      </form>
+      <section className="search full">
+        <div className="container flex-center">
+          <form className="search-form">
+            <input 
+              name="search"
+              type="text"
+              placeholder="Web Developer"
+            />
+          </form>
+        </div>
+      </section>
     );
   }
 }
